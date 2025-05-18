5f0c55e154b659b9d37e726e8577e1cb9c905845
Cole Neville <5910739+ColeNeville@users.noreply.github.com>
2025-05-17 22:37:23 -0600

    chore(doom): remove config.el from git tracking and add to .gitignore

--- doom-emacs/.config/doom/config.el
+++
@@ -1,9 +0,0 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; This file is auto-generated from config.org.
-;; Please edit that file instead.
-
-(org-babel-load-file
- (expand-file-name
-  "config.org"
-  doom-private-dir))
