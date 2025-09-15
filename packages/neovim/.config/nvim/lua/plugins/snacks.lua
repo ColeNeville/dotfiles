adf560fe7bb068c5f8c1691b0c99cbd2c111c172
Cole Neville <5910739+ColeNeville@users.noreply.github.com>
2025-09-15 13:01:39 -0600

    fix: remove telescope and project.nvim from nevim

--- packages/neovim/.config/nvim/lua/plugins/snacks.lua
+++ packages/neovim/.config/nvim/lua/plugins/snacks.lua
@@ -1,5 +1,14 @@
 return {
   "folke/snacks.nvim",
+  keys = {
+    {
+      "<leader>pp",
+      function()
+        Snacks.picker.projects()
+      end,
+      desc = "Open Project",
+    },
+  },
   opts = {
     picker = {
       hidden = true,
