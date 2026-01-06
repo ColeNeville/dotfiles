7f96138b3039826870247311f97feabd0d8ec6de
Cole Neville <5910739+ColeNeville@users.noreply.github.com>
2026-01-06 09:39:18 -0700

    fix: local config on wezterm

--- packages/common/.config/wezterm/wezterm.lua
+++ packages/common/.config/wezterm/wezterm.lua
@@ -1,21 +1,34 @@
+function file_exists(name)
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
 local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("CaskaydiaCove Nerd Font")
 config.font_size = 14
 
-config.color_scheme = "GruvboxDarkHard"
+config.color_scheme = "GruvboxDark"
 
 -- In my workflow multiplexing will be handled by tmux
 config.hide_tab_bar_if_only_one_tab = true
 
--- Host specific configuration
-local hostname = os.getenv("HOSTNAME")
-if hostname == "garuda-v6" then
-	config.enable_wayland = false
-	config.font_size = 12
-end
+local local_config = wezterm.config_dir .. "/local_config.lua"
 
-config.default_prog = { "tmux" }
+-- Load local overrides if present
+if file_exists(local_config) then
+  local local_config = dofile(local_config)
+  wezterm.log_info(local_config)
+
+  for k, v in pairs(local_config) do
+    config[k] = v
+  end
+end
 
 return config
