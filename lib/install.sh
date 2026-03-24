6040ff7172451d6a1f7a4943c0c926383d5948f6
Cole Neville <5910739+ColeNeville@users.noreply.github.com>
2026-03-23 19:46:49 -0600

    fix: issues with paru install when array is empty

--- lib/install.sh
+++ lib/install.sh
@@ -10,38 +10,44 @@ PARU_PACKAGES=${PARU_PACKAGES:-()}
 HOMEBREW_PACKAGES=${HOMEBREW_PACKAGES:-()}
 
 function install_pacman_packages {
-  log_info "Installing packages using pacman..."
-  sudo pacman -Sy --noconfirm "${PACMAN_PACKAGES[@]}" &>/dev/null
-  log_info "Pacman package installation completed."
+	if [[ ${#PACMAN_PACKAGES[@]} -eq 0 ]]; then
+		return 0
+	fi
+	log_info "Installing packages using pacman..."
+	sudo pacman -Sy --noconfirm "${PACMAN_PACKAGES[@]}" 1>/dev/null
+	log_info "Pacman package installation completed."
 }
 
 function install_homebrew_packages {
-  log_info "Installing packages using Homebrew..."
-  for package in "${HOMEBREW_PACKAGES[@]}"; do
-    if ! brew list --formula | grep -q "^${package}\$"; then
-      brew install "$package" &>/dev/null
-      log_info "Installed $package via Homebrew."
-    else
-      log_info "$package is already installed via Homebrew."
-    fi
-  done
-  log_info "Homebrew package installation completed."
+	log_info "Installing packages using Homebrew..."
+	for package in "${HOMEBREW_PACKAGES[@]}"; do
+		if ! brew list --formula | grep -q "^${package}\$"; then
+			brew install "$package" 1>/dev/null
+			log_info "Installed $package via Homebrew."
+		else
+			log_info "$package is already installed via Homebrew."
+		fi
+	done
+	log_info "Homebrew package installation completed."
 }
 
 if command -v pacman &>/dev/null; then
-  install_pacman_packages
+	install_pacman_packages
 elif command -v brew &>/dev/null; then
-  install_homebrew_packages
+	install_homebrew_packages
 else
-  log_error "No supported package manager found (pacman or Homebrew)."
+	log_error "No supported package manager found (pacman or Homebrew)."
 fi
 
 function install_paru_packages {
-  log_info "Installing packages using paru..."
-  paru -Sy --noconfirm "${PARU_PACKAGES}" &>/dev/null
-  log_info "Paru packages installation completed."
+	if [[ ${#PARU_PACKAGES[@]} -eq 0 ]]; then
+		return 0
+	fi
+	log_info "Installing packages using paru..."
+	paru -Sy --noconfirm "${PARU_PACKAGES[@]}" 1>/dev/null
+	log_info "Paru packages installation completed."
 }
 
 if command -v paru &>/dev/null; then
-  install_paru_packages
+	install_paru_packages
 fi
